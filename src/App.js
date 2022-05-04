ac6b0e9585d7639686d4b5ec4f66a1c24e28e6f1
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 01:48:26 +0200

    add feedbackStats component to our app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import FeedBackList from "./components/Feedbacklist/FeedBackList";
 import Header from "./components/Header/Header";
 import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats/FeedbackStats";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
   const deleteFeedback = (id) => {
@@ -15,6 +16,7 @@ function App() {
     <>
       <Header />
       <div className="App">
+        <FeedbackStats feedback={feedback} />
         <FeedBackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
