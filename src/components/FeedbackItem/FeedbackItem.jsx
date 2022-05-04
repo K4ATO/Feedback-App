190118ecd255d2a52d07ecdc89865b312b862d34
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 00:57:53 +0200

    add feedback item

---
+++ src/components/FeedbackItem/FeedbackItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useState} from 'react'
+
+
+
+const FeedbackItem = () => {
+
+    const [rating,setRating] = useState(7);
+    const [text,setText] = useState('and example of a feedback item');
+  return (
+    <div className='card'>
+        <div className="num-display">{rating}</div>
+        <div className="text-display">
+            {text}
+        </div>
+    </div>
+  )
+}
+
+export default FeedbackItem
