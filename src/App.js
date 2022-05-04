4c678f62de000a8bd56b889ae187599669c99027
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 01:15:43 +0200

    add feedbacklist component to our app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
-import FeedbackItem from "./components/FeedbackItem/FeedbackItem";
+import FeedBackList from "./components/Feedbacklist/FeedBackList";
 import Header from "./components/Header/Header";
-
+import { useState } from "react";
+import FeedbackData from "./data/FeedbackData";
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header />
       <div className="App">
-        <FeedbackItem />
+        <FeedBackList feedback={feedback} />
       </div>
     </>
   );
