64a1db01cb0622af240940e2590cfb97b52c2825
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 02:44:14 +0200

    add real time validation on review input

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -5,8 +5,20 @@ import Button from "../shared/Button";
 
 const FeedbackForm = () => {
   const [text, setText] = useState("");
+  const [btnDisabled, setbtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if (text === "") {
+      setbtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length < 9) {
+      setMessage("Text must be at least 10 characters");
+      setbtnDisabled(true);
+    } else {
+      setMessage(null);
+      setbtnDisabled(false);
+    }
     setText(e.target.value);
   };
   return (
@@ -20,8 +32,11 @@ const FeedbackForm = () => {
             placeholder="write a review"
             value={text}
           ></input>
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
