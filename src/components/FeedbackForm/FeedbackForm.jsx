d49c23c0f3a772a61a16647a68150d45404dffb4
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 05:16:05 +0200

    remove json server from project

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,75 +1,88 @@
-import React from "react";
-import Card from "../shared/Card";
 import { useState, useContext, useEffect } from "react";
+import RatingSelect from "../Rating/Rating";
+import Card from "../shared/Card";
 import Button from "../shared/Button";
-import Rating from "../Rating/Rating";
 import FeedbackContext from "../../context/FeedbackContext";
 
-const FeedbackForm = () => {
+function FeedbackForm() {
   const [text, setText] = useState("");
   const [rating, setRating] = useState(10);
-  const [btnDisabled, setbtnDisabled] = useState(true);
+  const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
+
   const { addFeedback, feedbackEdit, updateFeedback } =
     useContext(FeedbackContext);
 
   useEffect(() => {
     if (feedbackEdit.edit === true) {
-      setbtnDisabled(false);
+      setBtnDisabled(false);
       setText(feedbackEdit.item.text);
       setRating(feedbackEdit.item.rating);
     }
   }, [feedbackEdit]);
-  const handleTextChange = (e) => {
-    if (text === "") {
-      setbtnDisabled(true);
-      setMessage(null);
-    } else if (text !== "" && text.trim().length < 9) {
-      setMessage("Text must be at least 10 characters");
-      setbtnDisabled(true);
+
+  // NOTE: This should be checking input value not state as state won't be the updated value until the next render of the component
+
+  // prettier-ignore
+  const handleTextChange = ({ target: { value } }) => { // 👈  get the value
+    if (value === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+      
+  // prettier-ignore
+    } else if (value.trim().length < 10) { // 👈 check for less than 10
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
     } else {
-      setMessage(null);
-      setbtnDisabled(false);
+      setMessage(null)
+      setBtnDisabled(false)
     }
-    setText(e.target.value);
-  };
+    setText(value)
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (text.trim().length > 9) {
+    if (text.trim().length > 10) {
       const newFeedback = {
         text,
         rating,
       };
+
       if (feedbackEdit.edit === true) {
         updateFeedback(feedbackEdit.item.id, newFeedback);
       } else {
         addFeedback(newFeedback);
       }
 
+      // NOTE: reset to default state after submission
+      setBtnDisabled(true); // 👈  add this line to reset disabled
+      setRating(10); //👈 add this line to set rating back to 10
       setText("");
     }
   };
+
+  // NOTE: pass selected to RatingSelect so we don't need local duplicate state
   return (
     <Card>
       <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-        <Rating select={(rating) => setRating(rating)} />
+        <RatingSelect select={setRating} selected={rating} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
             type="text"
-            placeholder="write a review"
+            placeholder="Write a review"
             value={text}
-          ></input>
+          />
           <Button type="submit" isDisabled={btnDisabled}>
             Send
           </Button>
         </div>
+
         {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
-};
+}
 
 export default FeedbackForm;
