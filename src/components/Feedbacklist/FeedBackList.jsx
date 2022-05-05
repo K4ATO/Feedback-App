0e2e21b113f1905f8be0f61072f45ba0b6300974
osama-salah-abdelmoniem <osama.s.abdelmoniem@gmail.com>
2022-05-05 03:21:27 +0200

    add animation to reviews

--- src/components/Feedbacklist/FeedBackList.jsx
+++ src/components/Feedbacklist/FeedBackList.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import FeedbackItem from '../FeedbackItem/FeedbackItem'
-const FeedBackList = ({feedback,handleDelete}) => {
-    if(!feedback || feedback.length === 0){
-        return <p>No Feedback Yet</p>
-    }
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import FeedbackItem from "../FeedbackItem/FeedbackItem";
+const FeedBackList = ({ feedback, handleDelete }) => {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
   return (
-    <div className='feedback-list'>
-        {feedback.map((item)=>(
-            <FeedbackItem key = {item.id} item = {item} handleDelete={handleDelete}/>
+    <div className="feedback-list">
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
         ))}
+      </AnimatePresence>
     </div>
-  )
-}
+  );
+};
 
-export default FeedBackList
+export default FeedBackList;
